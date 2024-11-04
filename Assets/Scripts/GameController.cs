a2e8caeb196377b87994ed79b25cac1b12fc5139
Purrplexe <piow375@gmail.com>
2024-11-04 12:23:13 +0100

    Added Intruments
    Cello added to scene, can play audio :)

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private string[] melodies = {
+        "Down",
+        "EineKliene",
+        "Melody 1",
+        "Melody 2",
+        "Melody 3",
+        "Melody 4",
+        "Sonata No 16",
+        "Up"
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
