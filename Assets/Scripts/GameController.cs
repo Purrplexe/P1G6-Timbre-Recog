1bffc3cf88a0d90d3c4ab1cfbcb7a4d5b3d8bcca
Purrplexe <piow375@gmail.com>
2024-11-11 20:05:23 +0100

    Update GameController.cs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    private string[] melodies = {
-        "Down",
-        "EineKliene",
-        "Melody 1",
-        "Melody 2",
-        "Melody 3",
-        "Melody 4",
-        "Sonata No 16",
-        "Up"
-    };
     // Start is called before the first frame update
     void Start()
     {
