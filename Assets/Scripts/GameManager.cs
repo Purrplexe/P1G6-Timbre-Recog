c6b8d0abbfee8c729dfaa6f6f113672d48f7ba4d
Vahigoku <106706476+Vahigoku@users.noreply.github.com>
2024-11-19 21:57:24 +0100

    prototype scene done, but should change the way things are handled, the manager and such

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,9 @@
+using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using static System.TimeZoneInfo;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,14 +14,18 @@ public class GameManager : MonoBehaviour
     // [SerializeField] private TMPro.TextMeshProUGUI timeText;
     // [SerializeField] private TMPro.TextMeshProUGUI scoreText;
     public GameObject background;
+    public GameObject transitionScreen;
     public GameObject Info1;
     public GameObject Info2;
     public GameObject Info3;
     public GameObject button1;
-    //public GameObject button2;
-
+    public GameObject guitarCanvas;
+    public GameObject buttonGuitar;
+    public GameObject buttonReturn;
     public GameObject cameraMain;
     public GameObject cameraGuitar;
+    public GameObject cameraGuitarGame;
+    public bool transitioned = false;
     // Hardcoded variables you may want to tune.
     //private float startingTime = 30f;
 
@@ -41,16 +48,22 @@ public class GameManager : MonoBehaviour
     {
         // Hide/show the UI elements we don't/do want to see.
         background.SetActive(true);
+        transitionScreen.SetActive(false);
         Info1.SetActive(true);
         Info2.SetActive(false);
         Info3.SetActive(false);
         button1.SetActive(true);
+        buttonGuitar.SetActive(false);
+        buttonReturn.SetActive(false);
         //button2.SetActive(false);
         cameraMain.SetActive(true);
         cameraGuitar.SetActive(false);
-
+        cameraGuitarGame.SetActive(false);
+        guitarCanvas.SetActive(false);
+        //transitioned = false;
 
     }
+    
     public void Canner()
     {
         background.SetActive(false);
@@ -60,9 +73,52 @@ public class GameManager : MonoBehaviour
         button1.SetActive(false);
         //button2.SetActive(true);
     }
-    public void MainToGuitar()
+    public void MainToGuitarSelect()
     {
         //cameraMain.SetActive(false);
         cameraGuitar.SetActive(true);
+        buttonGuitar.SetActive(true);
+        buttonReturn.SetActive(true);
+    }
+    public void PlayGuitar()
+    {
+        buttonGuitar.SetActive(false);
+        cameraGuitar.SetActive(false);
+        Info2.SetActive(false);
+        Info3.SetActive(false);
+        //transitionScreen.SetActive(true);
+        StartCoroutine (TransitionScreen());
+       
+            
+            cameraGuitarGame.SetActive(true);
+            guitarCanvas.SetActive(true);
+            buttonReturn.SetActive(true);
+            //transitioned = false;
+        
+        
+        
+        //transitionScreen.SetActive(false); 
+    }
+    public void ReturnToHub()
+    {
+        //transitionScreen.SetActive(true);
+        StartCoroutine(TransitionScreen());
+        cameraGuitarGame.SetActive(false);
+        guitarCanvas.SetActive(false);
+        cameraGuitar.SetActive(false);
+        cameraMain.SetActive(true);
+        buttonReturn.SetActive(false);
+        buttonGuitar.SetActive(false);
+        Info2.SetActive(true);
+        Info3.SetActive(true);
+        //transitionScreen.SetActive(false);
+    }
+    public IEnumerator TransitionScreen()
+    {
+        transitionScreen.SetActive(true);
+        //transitioned = true;
+        yield return new WaitForSeconds(0.5f);
+        transitionScreen.SetActive(false);
+        
     }
 }
