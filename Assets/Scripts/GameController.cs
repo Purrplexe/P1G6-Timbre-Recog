955b906c3acabfa3beffb0f29e7e1bc40e19bb31
Purrplexe <piow375@gmail.com>
2024-12-03 12:55:03 +0100

    fixed dumb bug
    buh

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -54,7 +54,7 @@ public class GameController : MonoBehaviour
                 {
                     randominstrument = Random.Range(0, instruments.Length);
                 }
-                currentInstruments[i] = Random.Range(0, instruments.Length);
+                currentInstruments[i] = randominstrument;
             }
             
         }
