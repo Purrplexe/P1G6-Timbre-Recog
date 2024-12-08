86bb2e0f4f1b86a827d162b5dfa0eab31ce3a04a
Purrplexe <piow375@gmail.com>
2024-12-08 14:58:58 +0100

    added difficulty class

---
+++ Assets/Scripts/Difficulty.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Difficulty : MonoBehaviour
+{
+    int difficulty;
+    string[] instrument;
+    bool isDifferentMelodies;
+
+}
